c81877431d289bc37b493f246883df9d2ba8e4a0
lucasgroba <lucasgroba9@hotmail.com>
2018-08-30 13:41:43 -0300

    Comit con capa de servicios incluida

---
+++ PracticoTSI1/BusinessLogicLayer/BLEmployees.cs
@@ -0,0 +1,75 @@
+﻿using DataAccessLayer;
+using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class BLEmployees : IBLEmployees
+    {
+        private IDALEmployees _dal;
+        public BLEmployees(IDALEmployees dal)
+        {
+            _dal = dal;
+        }
+        public void AddEmployee(Employee emp)
+        {
+            if (emp != null)
+            {
+                _dal.AddEmployee(emp);
+            }
+
+        }
+        public void DeleteEmployee(int id)
+        {
+            if (id != 0)
+            {                _dal.DeleteEmployee(id);
+            }
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            if (emp != null)
+            {
+                _dal.UpdateEmployee(emp);
+            }
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+
+            return _dal.GetAllEmployees();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+
+            return _dal.GetEmployee(id);
+        }
+
+        public double CalcPartTimeEmployeeSalary(int idEmployee, int hours)
+        {
+            if (idEmployee != 0)
+            {
+                PartTimeEmployee emp = (PartTimeEmployee)_dal.GetEmployee(idEmployee);
+                if (emp != null)
+                {
+
+                    return emp.SalXHora * hours;
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ PracticoTSI1/DataAccessLayer/DALEmployeesEF.cs
@@ -0,0 +1,151 @@
+﻿using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DALEmployeesEF : IDALEmployees
+    {
+        public void AddEmployee(Employee emp)
+        {
+            using (Model.TESTEntities DB = new Model.TESTEntities())
+            {
+                if (emp is PartTimeEmployee)
+                {
+                    PartTimeEmployee empLP = new PartTimeEmployee();
+                    Model.PartTimeEmployee nuevo = new Model.PartTimeEmployee();
+                    nuevo.EmployeeId = empLP.Id;
+                    nuevo.Name = empLP.Name;
+                    nuevo.StartDate = empLP.StartDate;
+                    nuevo.HourlyRate = empLP.HourlyRate;
+                    DB.Employees.Add(nuevo);
+
+                }
+                else {
+                    FullTimeEmployee empLP = new FullTimeEmployee();
+                    Model.FullTimeEmployee nuevo = new Model.FullTimeEmployee();
+                    nuevo.EmployeeId = empLP.Id;
+                    nuevo.Name = empLP.Name;
+                    nuevo.StartDate = empLP.StartDate;
+                    nuevo.Salary = empLP.Salary;
+                    DB.Employees.Add(nuevo);
+                }
+                DB.SaveChanges();
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            using (Model.TESTEntities DB = new Model.TESTEntities())
+            {
+                DB.Employees.Remove(DB.Employees.Find(id));
+                DB.SaveChanges();
+
+            }
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            using (Model.TESTEntities DB = new Model.TESTEntities()) {
+                if(emp != null)
+                {
+                    if (emp != null) {
+                            Model.Employee EmpDB =DB.Employees.Find(emp.Id);
+                        if (EmpDB is Model.PartTimeEmployee) {
+                            PartTimeEmployee empLP = (PartTimeEmployee)emp;
+                            Model.PartTimeEmployee nuevo = (Model.PartTimeEmployee)EmpDB;
+                            nuevo.Name = empLP.Name;
+                            nuevo.StartDate = empLP.StartDate;
+                            nuevo.HourlyRate = empLP.HourlyRate;
+                            DB.Employees.Attach(nuevo);
+
+                        }
+                        else
+                        {
+                            FullTimeEmployee empLP = (FullTimeEmployee)emp;
+                            Model.FullTimeEmployee nuevo = (Model.FullTimeEmployee)EmpDB;
+                            nuevo.Name = empLP.Name;
+                            nuevo.StartDate = empLP.StartDate;
+                            nuevo.Salary = empLP.Salary;
+                            DB.Employees.Attach(nuevo);
+                        }
+                        DB.SaveChanges();
+
+                    }
+
+                }
+            }
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> lista = new List<Employee>();
+            using (Model.TESTEntities DB = new Model.TESTEntities())
+            {
+                var ListEmp = (from e in DB.Employees select e).ToList() ;
+                foreach (Model.Employee emp in ListEmp) {
+                    if (emp is Model.PartTimeEmployee)
+                    {
+                        Model.PartTimeEmployee empDB = (Model.PartTimeEmployee)emp;
+                        PartTimeEmployee nuevo = new PartTimeEmployee();
+                        nuevo.Id = empDB.EmployeeId;
+                        nuevo.Name = empDB.Name;
+                        nuevo.StartDate = empDB.StartDate;
+                        nuevo.HourlyRate = (Double)empDB.HourlyRate;
+                        lista.Add(nuevo);
+
+
+                    }
+                    else {
+                        Model.FullTimeEmployee empDB = (Model.FullTimeEmployee)emp;
+                        FullTimeEmployee nuevo = new FullTimeEmployee();
+                        nuevo.Id = empDB.EmployeeId;
+                        nuevo.Name = empDB.Name;
+                        nuevo.StartDate = empDB.StartDate;
+                        nuevo.Salary = (int)empDB.Salary;
+                        lista.Add(nuevo);
+                    }
+                }
+            }
+            return lista;
+
+            
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            using (Model.TESTEntities DB = new Model.TESTEntities())
+            {
+                var ListEmp = (from e in DB.Employees where e.EmployeeId == id select e).ToList();
+                foreach (Model.Employee emp in ListEmp){
+                    if (emp is Model.PartTimeEmployee){
+
+                        Model.PartTimeEmployee empDB = (Model.PartTimeEmployee)emp;
+                        PartTimeEmployee nuevo = new PartTimeEmployee();
+                        nuevo.Id = empDB.EmployeeId;
+                        nuevo.Name = empDB.Name;
+                        nuevo.StartDate = empDB.StartDate;
+                        nuevo.HourlyRate = (Double)empDB.HourlyRate;
+                        return nuevo;
+
+
+                    }
+                    else
+                    {
+                        Model.FullTimeEmployee empDB = (Model.FullTimeEmployee)emp;
+                        FullTimeEmployee nuevo = new FullTimeEmployee();
+                        nuevo.Id = empDB.EmployeeId;
+                        nuevo.Name = empDB.Name;
+                        nuevo.StartDate = empDB.StartDate;
+                        nuevo.Salary = (int)empDB.Salary;
+                        return nuevo;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
