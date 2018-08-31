d2fa5073fc601c9aa5b2b008472c5cf41222509f
Unknown <lucasgroba9@hotmail.com>
2018-08-30 23:14:31 -0300

    servicios publicados aun sin desacoplar

--- PracticoTSI1/ServiceLayer/Program.cs
+++ PracticoTSI1/ServiceLayer/Program.cs
@@ -24,11 +24,46 @@ namespace ServiceLayer
             blHandler = new BLEmployees(new DataAccessLayer.DALEmployeesEF());
         }
 
-        private static void SetupService()
+    private static void SetupService()
         {
-            ServiceHost host = new ServiceHost(typeof(ServiceEmployees), new Uri("http://localhost:8834/tsi1"));
-            host.AddServiceEndpoint(typeof(IServiceEmployees), new BasicHttpBinding(), "Soap");
-            host.Open();
+
+            ServiceHost svcHost = new ServiceHost(typeof(ServiceEmployees), new Uri("http://localhost:8834/tsi1/"));
+            try
+            {
+                // Check to see if the service host already has a ServiceMetadataBehavior
+                ServiceMetadataBehavior smb = svcHost.Description.Behaviors.Find<ServiceMetadataBehavior>();
+                // If not, add one
+                if (smb == null)
+                    smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                svcHost.Description.Behaviors.Add(smb);
+                // Add MEX endpoint
+                svcHost.AddServiceEndpoint(
+                  ServiceMetadataBehavior.MexContractName,
+                  MetadataExchangeBindings.CreateMexHttpBinding(),
+                  "mex"
+                );
+                // Add application endpoint
+                svcHost.AddServiceEndpoint(typeof(IServiceEmployees), new WSHttpBinding(), "");
+                // Open the service host to accept incoming calls
+                svcHost.Open();
+
+                // The service can now be accessed.
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                // Close the ServiceHostBase to shutdown the service.
+                svcHost.Close();
+            }
+            catch (CommunicationException commProblem)
+            {
+                Console.WriteLine("There was a communication problem. " + commProblem.Message);
+                Console.Read();
+            }
         }
     }
+    
 }
